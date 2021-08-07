6918cf78f6ced5c1a809ff4fd63ad0b2da0e3142
aarushi353 <aarushiabrol999@gmail.com>
2021-08-07 20:27:33 +0530

    Fourth Commit

---
+++ Portals, Fragments and Refs/src/components/Users/AddUser.js
@@ -0,0 +1,68 @@
+import React, { useState, useRef } from "react";
+import Wrapper from "../Helpers/Wrapper";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
+import classes from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+  const [error, setError] = useState();
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
+      return;
+    }
+    if (+enteredUserAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
+      return;
+    }
+    props.onAddUser(enteredName, enteredUserAge);
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = ''; 
+
+  };
+
+ 
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <Wrapper>
+      {error && (
+        <ErrorModal
+          key="error-modal"
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card key="Card-key" className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" ref={nameInputRef} />
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="number" ref={ageInputRef} />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </Wrapper>
+  );
+};
+
+export default AddUser;
+//If u decide to not use anything like div or any other covering element in your return statement, then simply use Array and don't forget to add key to each of this element.
+//Wrapper type components can be made
+//<React.Fragment></React.Fragment> or use <></> for serving JSX's limitation. import {Fragment} too from react if u use <Fragment></Fragment>.
