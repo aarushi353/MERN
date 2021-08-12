ffb5941da04ff3572579127122ffd31139bd734f
aarushi353 <aarushiabrol999@gmail.com>
2021-08-12 23:03:37 +0530

    Eighth Commit

--- Hooks/useReducer/src/components/Login/Login.js
+++ Hooks/useReducer/src/components/Login/Login.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useReducer } from "react";
-
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
@@ -65,17 +64,10 @@ const Login = (props) => {
   const emailChangeHandler = (event) => {
     dispatchEmail({type: 'USER_INPUT', val: event.target.value});
 
-    // setFormIsValid(
-    //   event.target.value.includes("@") && passwordState.isValid
-    // );
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({type: 'USER_INPUT', val: event.target.value})
-
-    // setFormIsValid(
-    //   emailState.isValid && event.target.value.trim().length > 6
-    // );
   };
 
   const validateEmailHandler = () => {
